5da291c1902fd639123bebd997fce4b06c005876
fmsunyh <fmsunyh@gmail.com>
2023-12-11 11:23:39 +0800

    example-09

--- example-09/make_gird.py
+++ example-09/make_gird.py
@@ -5,3 +5,14 @@
 # @Software: ZJ_AI
 # -----------------------------------------------------
 # -*- coding: utf-8 -*-
+import torch
+
+
+def _make_grid(nx=20, ny=20):
+    yv, xv = torch.meshgrid([torch.arange(ny), torch.arange(nx)])
+    return torch.stack((xv, yv), 2).view((1, 1, ny, nx, 2)).float()
+
+
+if __name__ == '__main__':
+    value = _make_grid()
+    print(value)
